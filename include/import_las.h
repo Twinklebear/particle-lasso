2cb9fbc3a3eeee88a911d31ba55b84b46f03256b
Will Usher <willusher.life@gmail.com>
2016-01-18 11:39:50 -0700

    Switch to LAStools for loading las/laz

--- include/import_las.h
+++ include/import_las.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <vector>
-#include <liblas/liblas.hpp>
 #include "types.h"
 
 void import_las(const FileName &file_name, ParticleModel &model);
--- src/import_las.cpp
+++ src/import_las.cpp
@@ -1,60 +1,51 @@
+#include <limits>
+#include <lasreader.hpp>
 #include "types.h"
 #include "import_las.h"
 
 void import_las(const FileName &file_name, ParticleModel &model){
-	std::ifstream fin(file_name.file_name.c_str(), std::ios::in | std::ios::binary);
-	liblas::ReaderFactory rf;
-	liblas::Reader reader = rf.CreateWithStream(fin);
+	LASreadOpener read_opener;
+	read_opener.set_file_name(file_name.file_name.c_str());
+	LASreader *reader = read_opener.open();
+	std::cout << "Reading LIDAR data set from '" << file_name
+		<< "' with " << reader->npoints << " points\n"
+		<< "min = ( " << reader->get_min_x()
+		<< ", " << reader->get_min_y()
+		<< ", " << reader->get_min_z() << " )\n"
+		<< "max = ( " << reader->get_max_x()
+		<< ", " << reader->get_max_y()
+		<< ", " << reader->get_max_z() << " )\n";
 
-	const liblas::Header &header = reader.GetHeader();
-	std::cout << "Reading LIDAR data set from " << file_name
-		<< " with " << header.GetPointRecordsCount()
-		<< " points\n";
-	//Offsets we need to apply to position the points properly
-	const vec3f scale = vec3f(header.GetScaleX(), header.GetScaleY(), header.GetScaleZ());
-	const vec3f offset = vec3f(header.GetOffsetX(), header.GetOffsetY(), header.GetOffsetZ());
-	const vec3f min_pt = vec3f(header.GetMinX(), header.GetMinY(), header.GetMinZ())
-		* scale + offset;
-	const vec3f max_pt = vec3f(header.GetMaxX(), header.GetMaxY(), header.GetMaxZ())
-		* scale + offset;
-	std::cout << "scale: " << scale << ", offset: " << offset
-		<< "\nmin = " << min_pt
-		<< "\nmax = " << max_pt << "\n";
-
-	bool has_color = header.GetDataFormatId() == liblas::ePointFormat2
-		|| header.GetDataFormatId() == liblas::ePointFormat3
-		|| header.GetDataFormatId() == liblas::ePointFormat5;
+	bool has_color = reader->header.point_data_format == 2
+		|| reader->header.point_data_format == 3
+		|| reader->header.point_data_format == 5;
 
 	auto positions = std::make_unique<DataT<float>>();
 	auto colors = std::make_unique<DataT<uint8_t>>();
-	positions->data.reserve(header.GetPointRecordsCount() * 3);
-	colors->data.reserve(header.GetPointRecordsCount());
-	int n_discarded = 0;
-	const float inv_max_color = 1.f / 65536.f;
-	// Unfortunately the libLAS designers prefer Java style iterators
-	while (reader.ReadNextPoint()){
-		const liblas::Point &lasp = reader.GetPoint();
+	positions->data.reserve(reader->npoints * 3);
+	colors->data.reserve(reader->npoints);
+	const float inv_max_uint16 = 1.f / std::numeric_limits<uint16_t>::max();
+	size_t n_discarded = 0;
+	while (reader->read_point()){
 		// Points classified as low point are noise and should be discarded
-		if (lasp.GetClassification().GetClass() == liblas::Point::eLowPoint){
+		if (reader->point.get_classification() == 7){
 			++n_discarded;
 			continue;
 		}
-		const liblas::Color lasc = lasp.GetColor();
-		const vec3f p = vec3f(lasp.GetX(), lasp.GetY(), lasp.GetZ()) * scale + offset;
-		vec3f c;
+		reader->point.compute_coordinates();
+		for (size_t i = 0; i < 3; ++i){
+			positions->data.push_back(reader->point.coordinates[i]);
+		}
 		if (has_color){
-			c = vec3f(lasc.GetRed() * inv_max_color, lasc.GetGreen() * inv_max_color,
-					lasc.GetBlue() * inv_max_color);
+			const uint16_t *rgba = reader->point.get_rgb();
+			for (size_t i = 0; i < 4; ++i){
+				colors->data.push_back(static_cast<uint8_t>(255.0 * rgba[i] * inv_max_uint16));
+			}
+		} else {
+			for (size_t i = 0; i < 4; ++i){
+				colors->data.push_back(255);
+			}
 		}
-		else {
-			c = vec3f(1.0);
-		}
-		positions->data.push_back(p.x);
-		positions->data.push_back(p.y);
-		positions->data.push_back(p.z);
-		colors->data.push_back(static_cast<uint8_t>(c.x * 255.0));
-		colors->data.push_back(static_cast<uint8_t>(c.y * 255.0));
-		colors->data.push_back(static_cast<uint8_t>(c.z * 255.0));
 	}
 	std::cout << "Discarded " << n_discarded << " noise classified points\n"
 		<< "Will save " << positions->data.size() / 3 << " points to output\n";
