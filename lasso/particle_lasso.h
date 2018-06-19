36c5df90493180ec516888ce20b097334690ee63
Will Usher <willusher.life@gmail.com>
2018-06-19 15:33:25 -0700

    add a bundle wrapper header

---
+++ lasso/particle_lasso.h
@@ -0,0 +1,10 @@
+#include "particle_lasso_cfg.h"
+#include "types.h"
+#include "import_scivis16.h"
+#include "import_uintah.h"
+#include "import_xyz.h"
+
+#ifdef PARTICLE_LASSO_ENABLE_LIDAR
+#include "import_las.h"
+#endif
+
